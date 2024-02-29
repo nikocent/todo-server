d222301dcfb4203faaf3096e75aa7e49159374c2
Nicolas Centeno O <nicocenteno202@gmail.com>
2024-02-29 05:34:36 -0300

    Primer commit y conexión a DB

---
+++ src/main/java/dev/nicocent202/todos/TodosApplication.java
@@ -0,0 +1,19 @@
+package dev.nicocent202.todos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TodosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodosApplication.class, args);
+	}
+	@GetMapping("/")
+	public String hello(){
+		return "Hello World";
+	}
+}
